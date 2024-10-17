d43f276cb3c3483d9ad71a029a6e8ed3828da6d6
Kecyke <kecyke@Kecyke-Laptop-D.Kecyke>
2024-10-17 13:29:50 +0200

    build kivétele

--- build/Desktop-Debug/CMakeFiles/appscatterplot_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".rcc/qmlcache/appscatterplot_qmlcache_loader.cpp"
-  "CMakeFiles/appscatterplot_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/appscatterplot_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/Desktop-Debug/CMakeFiles/appscatterplot_tooling.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "scatterplot/Main.qml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/appscatterplot_tooling.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
